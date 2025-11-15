a10a1a7d919745694cd0e6d0b5b36147f8a15505
mh bakhshipour <mh.bakhshipour@yahoo.com>
2025-11-15 22:12:30 +0330

    feat: Optimize TransactionViewSet queryset to use select_related for improved performance

--- transaction/views/transaction.py
+++ transaction/views/transaction.py
@@ -5,7 +5,7 @@ from transaction.serializers import TransactionSerializer
 
 
 class TransactionViewSet(BaseModelViewSet):
-    queryset = Transaction.objects.all()
+    queryset = Transaction.objects.select_related("vendor", "customer").all()
     serializer_class = TransactionSerializer
     search_fields = ["vendor_name", "customer_phone_number"]
 
